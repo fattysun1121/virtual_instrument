99eeea1c91f99ba2cbebaf2cf4de4502480a01f3
Elliot Hipski <elhip03@gmail.com>
2022-11-12 12:14:48 -0500

    Added a custom window class to store attributes

--- GUI.py
+++ GUI.py
@@ -6,6 +6,7 @@ Cal Tumminello
 
 # import tkinter as tk
 import sys
+
 import cv2
 #from PIL import ImageTk, Image
 
@@ -14,12 +15,44 @@ if (sys.platform == "linux" or sys.platform == "linux2"):
     from tk import * 
 elif (sys.platform == "win64"):
     import tkinter as tk
+    from tkinter import Toplevel
 elif (sys.platform == 'darwin'):
     import tkinter as tk
+    from tkinter import Toplevel
+    from enum import Enum
+    from tkinter import Frame
+# enumeration for instrument type
+
+class instruments(Enum):
+
+    BONGOS = 1
+    THERAMIN = 2
+    COMPOSER = 3
+
+
+    
+
+
+
+# toplevel controller for window groups
+
+top = Toplevel(bg="black", fg="white", )
 
 
 #  window for windows users
+
+class InstrumentWindow(Frame):
+    def __init__(self) -> None:
+        super().__init__()
+        self.instrument = None
+        self.
+
+
+
+
 window = tk.Tk()
+
+
 
 '''
 Widgets
@@ -37,6 +70,8 @@ instruList = tk.Listbox(bg='gray')
 instruList.insert(1, "Bongos")
 instruList.insert(2, "Guitar")
 instruList.insert(3, "Theremin")
+
+instruList.pack()
 
 # 
 
