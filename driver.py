f251bc7b958efbcfe6e65713c42da0e13bff3c18
fattysun1121 <fatharley2001@gmail.com>
2022-11-12 13:02:25 -0500

    Update driver.py and FrameProcessor to debug FrameProcessor

--- driver.py
+++ driver.py
@@ -4,13 +4,12 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-from lib import FrameProcessor
+from lib import FrameProcessor as fp
 
 class Driver:
 	def __init__(self):
 		self.introduction() 
-		# initialize Pose estimator
-
+		self.processor = fp.FrameProcessor()
 
 
 	# Run the program by the input camera type 
@@ -21,19 +20,38 @@ class Driver:
 			# Start a pipeline representing the bgr camera
 			pipe = rs.pipeline()
 			config = rs.config()
-			config.enable_stream(rs.stream.color, rs.format.bgr8, 30)
+			config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 			pipe.start(config)
+
+			while True:
+				# Get BGR frame
+			    frames = pipe.wait_for_frames()
+			    color = frames.get_color_frame()
+
+			    if not color: continue
+
+			    color_array = np.asanyarray(color.get_data())
+			    
+
+			    self.processor.process_frame(color_array)
+			    
+			    # show the final output
+			    cv2.imshow('Output', color_array)
+			    
+			    if cv2.waitKey(1) == ord('q'):
+			        break
+
+			pipe.stop()
 
 		elif camera_type == 'kinect':
 			from freenect import sync_get_depth as get_depth, sync_get_video as get_video
 		else:
 			print('Camera not supported')
 		
-
+ 
 	# Play the instrument based on the passed string arg
 	def play_instrument(self, instrument):
-		
-
+		pass
 	
 	@staticmethod
 	def introduction():
@@ -42,6 +60,7 @@ class Driver:
 
 
 driver = Driver()
+driver.run('realsense')
 
 
 
--- lib/FrameProcessor.py
+++ lib/FrameProcessor.py
@@ -3,13 +3,14 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import time
 
 class FrameProcessor:
     def __init__(self):
-        mp_drawing = mp.solutions.drawing_utils
-        mp_pose = mp.solutions.pose
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_pose = mp.solutions.pose
 
-        pose = mp_pose.Pose(
+        self.pose = self.mp_pose.Pose(
                 min_detection_confidence=0.5,
                 min_tracking_confidence=0.5)
 
@@ -21,12 +22,11 @@ class FrameProcessor:
         self.lhand_velocity = [0, 0, 0]
         self.lhand_accel    = [0, 0, 0]
         
-        self.t1 = 0
         self.t0 = 0
 
-    def draw_landmarks(self, frame):
-        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-        return
+    def draw_landmarks(self, frame, results):
+        self.mp_drawing.draw_landmarks(frame, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+        
 
     # Retrieve user's waste height
     def get_waist_height(self):
@@ -44,42 +44,42 @@ class FrameProcessor:
     def process_frame(self, frame):
 
         # Calculate delta t
-        self.dt = time.time() - t0
-        results = pose.process(rgb)
+        dt = time.time() - self.t0
+        results = self.pose.process(frame)
 
         # -1 indicates failure
         if not results.pose_landmarks:
             return -1
 
-        rhand_position_new = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST]
-        lhand_position_new = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST]
+        rhand_position_new = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_WRIST]
+        lhand_position_new = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_WRIST]
 
         # Calculate right hand kinematics
         rhand_velocity_new = [0, 0, 0]
-        rhand_velocity_new = (rhand_position_new.x - self.rhand_position[0]) / self.dt
-        rhand_velocity_new = (rhand_position_new.y - self.rhand_position[1]) / self.dt
-        rhand_velocity_new = (rhand_position_new.z - self.rhand_position[2]) / self.dt
+        rhand_velocity_new = (rhand_position_new.x - self.rhand_position[0]) / dt
+        rhand_velocity_new = (rhand_position_new.y - self.rhand_position[1]) / dt
+        rhand_velocity_new = (rhand_position_new.z - self.rhand_position[2]) / dt
 
-        self.rhand_accel[0] = (rhand_velocity_new[0] - self.rhand_velocity[0]) /self.dt
-        self.rhand_accel[1] = (rhand_velocity_new[1] - self.rhand_velocity[1]) /self.dt
-        self.rhand_accel[2] = (rhand_velocity_new[2] - self.rhand_velocity[2]) /self.dt
+        self.rhand_accel[0] = (rhand_velocity_new[0] - self.rhand_velocity[0]) /dt
+        self.rhand_accel[1] = (rhand_velocity_new[1] - self.rhand_velocity[1]) /dt
+        self.rhand_accel[2] = (rhand_velocity_new[2] - self.rhand_velocity[2]) /dt
 
         self.rhand_velocity = rhand_velocity_new
         self.rhand_position = rhand_position_new
 
         # Calculate left hand kinematics
         lhand_velocity_new = [0, 0, 0]
-        lhand_velocity_new = (lhand_position_new.x - self.lhand_position[0]) / self.dt
-        lhand_velocity_new = (lhand_position_new.y - self.lhand_position[1]) / self.dt
-        lhand_velocity_new = (lhand_position_new.z - self.lhand_position[2]) / self.dt
+        lhand_velocity_new = (lhand_position_new.x - self.lhand_position[0]) / dt
+        lhand_velocity_new = (lhand_position_new.y - self.lhand_position[1]) / dt
+        lhand_velocity_new = (lhand_position_new.z - self.lhand_position[2]) / dt
 
-        self.lhand_accel[0] = (lhand_velocity_new[0] - self.lhand_velocity[0]) /self.dt
-        self.lhand_accel[1] = (lhand_velocity_new[1] - self.lhand_velocity[1]) /self.dt
-        self.lhand_accel[2] = (lhand_velocity_new[2] - self.lhand_velocity[2]) /self.dt
+        self.lhand_accel[0] = (lhand_velocity_new[0] - self.lhand_velocity[0]) / dt
+        self.lhand_accel[1] = (lhand_velocity_new[1] - self.lhand_velocity[1]) / dt
+        self.lhand_accel[2] = (lhand_velocity_new[2] - self.lhand_velocity[2]) / dt
 
         self.lhand_velocity = lhand_velocity_new
         self.lhand_position = lhand_position_new
 
         # Draw skeleton on frame after processing
-        self.draw_landmarks(frame)
+        self.draw_landmarks(frame, results)
         return 0
