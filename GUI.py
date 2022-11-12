05a0074f4e448b3a617c315ac20e6f9908a3fcc5
ctumminello <ctumminello@rlogin.ct.vt.edu>
2022-11-11 21:14:05 -0500

    GUI v0.1

---
+++ GUI.py
@@ -0,0 +1,12 @@
+'''
+v0.1 - GUI
+'''
+import tkinter
+
+window = tkinter.Tk()
+
+# Code to add widgets will go here...
+window.mainloop()
+instruList = Listbox(master, options)
+instruList.yview()
+
