582d56a044100834721cafaaee4b745765ac1c1f
fattysun1121 <fatharley2001@gmail.com>
2022-11-12 11:55:14 -0500

    Set up skeleton of driver.py

---
+++ driver.py
@@ -0,0 +1,26 @@
+# Driver class drives the camera, GUI, and FrameProcessor. It gives FrameProcessor the frame, and receives
+# 
+
+import cv2
+import numpy
+from lib import frame_processor
+
+class Driver:
+	def __init__(self):
+		pass
+
+	# Run the program by the input camera type 
+	def run(self, camera_type):
+		pass
+
+	# Play the instrument based on the passed string arg
+	def play_instrument(self, instrument):
+		pass
+
+	
+	def function():
+		pass
+
+
+
+
