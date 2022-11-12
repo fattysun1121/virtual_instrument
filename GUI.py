59e762a35718413540b08f88d80f8d13584654f7
ctumminello <ctumminello@rlogin.ct.vt.edu>
2022-11-11 23:12:36 -0500

    GUI v0.2 trying to add openCV

--- GUI.py
+++ GUI.py
@@ -1,18 +1,43 @@
 '''
-v0.1 - GUI
+v0.2 - GUI
+
+Cal Tumminello
 '''
-import tkinter as tk
+
+# import tkinter as tk
+import sys
+import cv2
+from PIL import ImageTk, Image
+
+# Changes import based on os 
+if (sys.platform == "linux" or sys.platform == "linux2"):
+    from tk import *
+elif (sys.platform == "win32"):
+    import tkinter as tk
+
 window = tk.Tk()
 
-# Code to add widgets will go here...
+'''
+Widgets
+'''
 
 title = tk.Label(text="Instruments")
-instruList = tk.Listbox()
+
+# Basic buttons
+startButton = tk.Button()
+stopButton = tk.Button()
+recordButton = tk.Button()
+
+# List box buttons
+instruList = tk.Listbox(bg='gray')
 instruList.insert(1, "Bongos")
 instruList.insert(2, "Guitar")
 instruList.insert(3, "Theremin")
 
+# 
+
 title.pack()
 instruList.pack()
 
+# User
 window.mainloop()
