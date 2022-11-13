93e2a10db513de5c856b55543b48b1ce40861c10
Elliot Hipski <elhip03@gmail.com>
2022-11-13 06:21:06 -0500

    Fixed bug where vars not changed

--- GUI.py
+++ GUI.py
@@ -108,8 +108,13 @@ instrument_choice = StringVar()
 instrument_choice.set(camera_type_list[0])
 
 
-camera_menu = ttk.Combobox(root, value=camera_type_list)
-instrument_menu = ttk.Combobox(root, value=instrument_type_list)
+camera_menu = ttk.Combobox(root, value=camera_type_list, textvariable=camera_choice)
+instrument_menu = ttk.Combobox(root, value=instrument_type_list, textvariable=instrument_choice)
+
+instrument_choice.set(instrument_menu.get())
+
+camera_choice.set(camera_menu.get())
+
 
 
 title = Label(root, text="Handstruments", font=("Times New Roman", 25)).grid(row=0, column=1)
@@ -133,7 +138,7 @@ root.rowconfigure(3, weight=1)
 root.rowconfigure(4, weight=1)
 
 
-run_button = ttk.Button(root, text="Run").grid(column=1, row=2, sticky=(E, W))
+
 
 
 bongos_resize = Image.open("Bongos.PNG").resize(size=(100, 120))
@@ -155,6 +160,9 @@ theremin_label.grid(row=5,column=2, sticky=(W))
 def run():
 
     # TODO
-    print("runs the app")
+    print(camera_choice.get())
+
+run_button = ttk.Button(root, text="Run", command=run).grid(column=1, row=2, sticky=(E, W))
 
 root.mainloop()
+
