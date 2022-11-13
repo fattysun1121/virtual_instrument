e39580e715f0583b49eacf1c35e25b647282cc16
fattysun1121 <fatharley2001@gmail.com>
2022-11-13 00:07:22 -0500

    Set up basic GUI with two buttons of instruments

--- GUI.py
+++ GUI.py
@@ -8,15 +8,14 @@ class GUI:
 		mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 		root.columnconfigure(0, weight=1)
 		root.rowconfigure(0, weight=1)
-		ttk.Label(mainframe, text="Handstruments").grid(column=1, row=1, sticky=(N, W, E, S))
-		ttk.Button(mainframe, text="Bongos").grid(column=1, row=2, sticky=(E, W))
-		ttk.Button(mainframe, text="Theremin").grid(column=1, row=3, sticky=(E, W))
+		ttk.Label(mainframe, text="Handstruments").grid(row=1, columnspan=2, sticky=(W, E))
+		ttk.Button(mainframe, text="Bongos").grid(column=1, row=2, sticky=(W))
+		ttk.Button(mainframe, text="Theremin").grid(column=2, row=2, sticky=(E))
 
 	def hi(self):
 		print('haha')
  
 root = Tk()
 root.title("Handstruments")
-root.geometry("500x200")
 gui = GUI(root)
 root.mainloop()
